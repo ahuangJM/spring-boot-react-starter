d33afc4f4ba4c2f65ae865b87d7a49c33b6880f5
Alan <ahuang@indeed.com>
2023-02-05 18:19:44 -0700

    init project

---
+++ src/main/java/com/github/ahuangjm/spring/boot/react/starter/SpringBootReactStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.ahuangjm.spring.boot.react.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactStarterApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringBootReactStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ahuangjm/spring/boot/react/starter/SpringBootReactStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ahuangjm.spring.boot.react.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
